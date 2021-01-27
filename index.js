f0e1a74230cb5ab0e45627d8d72475bb7d7c0962
Tamirchn1@gmail.com <T2151991t>
2021-01-27 13:08:33 +0100

    inital commit

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-// const strings = document.querySelectorAll('.str');
-// const frets = document.querySelectorAll('.fret');
-//
-//
-//
-//
-// strings.forEach((string) => {
-//   string.addEventListener('mouseenter', () => {
-//     if ($('#myguitar').html() == "CLASSIC") {
-//       console.log(`${string.id}.mp3`)
-//       let sound = new Audio(`
-// ./sounds/${string.id}.mp3`)
-//       sound.play();
-//     } else if($('#myguitar').html('ELECTRIC')) {
-//       console.log(`e${string.id}.mp3`)
-//       let sound = new Audio(`
-// ./sounds/e${string.id}.mp3`)
-//       sound.play();
-//     }
-//     else {
-//       console.log(`c${string.id}.mp3`)
-//       let sound = new Audio(`
-//     ./sounds/c${string.id}.mp3`)
-//       sound.play();
-//     }
-//   })
-// });
-//
-//
-// $('#switch').on('click', function() {
-//   $('#myguitar').html() == "CLASSIC" ?
-//     $('#myguitar').html('ELECTRIC') :
-//     $('#myguitar').html('CLASSIC');
-// })
 
 
 const strings = document.querySelectorAll('.str');
